07b0a6573603dc8abe5688f82211d28283e3c22b
MCristhian7 <cristhianhuamani54@gmail.com>
2022-10-17 13:56:57 -0500

    Initial Commit

---
+++ MichaelColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MichaelColeccionDeJuegos
+//
+//  Created by Mac 06 on 17/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
